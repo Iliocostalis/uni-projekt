5a03f40524f265b6b62df6febe61e8d272c7c6ab
Iliocostalis <manuel.kohler.tt@gmail.com>
2022-06-09 21:18:52 +0200

    update

---
+++ libcamera/include/ipa/rkisp1_ipa_serializer.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: LGPL-2.1-or-later */
+/*
+ * Copyright (C) 2020, Google Inc.
+ *
+ * rkisp1_ipa_serializer.h - Image Processing Algorithm data serializer for rkisp1
+ *
+ * This file is auto-generated. Do not edit.
+ */
+
+#pragma once
+
+#include <tuple>
+#include <vector>
+
+#include <libcamera/ipa/rkisp1_ipa_interface.h>
+#include <libcamera/ipa/core_ipa_serializer.h>
+
+#include "libcamera/internal/control_serializer.h"
+#include "libcamera/internal/ipa_data_serializer.h"
+
+namespace libcamera {
+
+LOG_DECLARE_CATEGORY(IPADataSerializer)
+
+
+} /* namespace libcamera */
