62fdaa304e1191df57d26dff7b5a1d36672a72c1
Iliocostalis <manuel.kohler.tt@gmail.com>
2022-07-20 14:05:40 +0200

    update

--- imageProcessing.cpp
+++ imageProcessing.cpp
@@ -20,18 +20,24 @@ namespace ImageProcessing
         std::cout << "fps: " << 1.0f / seconds.count() << std::endl;
         std::cout << "size: " << size << std::endl;
 
-        if(val == 433333)
+        if(val == 30)
         {
             std::string fileName = std::string("image_") + std::to_string(val);
             
             auto myfile = std::ofstream(fileName, std::ios::out | std::ios::binary);
             myfile << "P6" << "\n" << 640 << " " << 480 << "\n" << 255 << "\n";
-            size = 640 * 480;
+            size = 640 * 480 / 2;
             for(int i = 0; i < size; ++i)
             {
-                myfile.write((char*)data[i], 1);
-                myfile.write((char*)data[i], 1);
-                myfile.write((char*)data[i], 1);
+                uint8_t valA = (data[i] & 0xf0) >> 4;
+                uint8_t valB = (data[i] & 0x0f);
+
+                myfile.write((char*)&valA, 1);
+                myfile.write((char*)&valA, 1);
+                myfile.write((char*)&valA, 1);
+                myfile.write((char*)&valB, 1);
+                myfile.write((char*)&valB, 1);
+                myfile.write((char*)&valB, 1);
             }
             myfile.close();
         }
