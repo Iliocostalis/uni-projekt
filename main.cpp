a8901d3a08d471971af686368431b978ebc2f904
Iliocostalis <manuel.kohler.tt@gmail.com>
2022-07-20 19:54:21 +0200

    update

--- main.cpp
+++ main.cpp
@@ -14,40 +14,40 @@ int main()
 {
 	ImageProcessing::init();
 
-	Display *display;
-    Window window;
-    XEvent event;
-    int screen;
-
-    /* open connection with the server */
-    display = XOpenDisplay(NULL);
-    if (display == NULL) 
-	{
-		fprintf(stderr, "Cannot open display\n");
-		exit(1);
-    }
-
-    screen = DefaultScreen(display);
-
-    /* create window */
-    window = XCreateSimpleWindow(display, RootWindow(display, screen), WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_BORDER, BlackPixel(display, screen), WhitePixel(display, screen));
-
-    /* process window close event through event handler so XNextEvent does not fail */
-    Atom del_window = XInternAtom(display, "WM_DELETE_WINDOW", 0);
-    XSetWMProtocols(display, window, &del_window, 1);
-
-    /* select kind of events we are interested in */
-    XSelectInput(display, window, ExposureMask | KeyPressMask);
-
-    /* display the window */
-    XMapWindow(display, window);
-
-    XFlush(display);
-    GC gc = XCreateGC(display, window, 0, NULL);
-
-	for(int i = 0; i < IMAGE_BUFFER_COUNT; ++i)
-		ImageProcessing::images[i] = XCreateImage(display, DefaultVisual(display, 0), 24, ZPixmap, 0, ImageProcessing::imageBuffer[i].data(), IMAGE_WIDTH, IMAGE_HEIGHT, 32, 0);
-
+	//Display *display;
+    //Window window;
+    //XEvent event;
+    //int screen;
+//
+    ///* open connection with the server */
+    //display = XOpenDisplay(NULL);
+    //if (display == NULL) 
+	//{
+	//	fprintf(stderr, "Cannot open display\n");
+	//	exit(1);
+    //}
+//
+    //screen = DefaultScreen(display);
+//
+    ///* create window */
+    //window = XCreateSimpleWindow(display, RootWindow(display, screen), WINDOW_X, WINDOW_Y, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_BORDER, BlackPixel(display, screen), WhitePixel(display, screen));
+//
+    ///* process window close event through event handler so XNextEvent does not fail */
+    //Atom del_window = XInternAtom(display, "WM_DELETE_WINDOW", 0);
+    //XSetWMProtocols(display, window, &del_window, 1);
+//
+    ///* select kind of events we are interested in */
+    //XSelectInput(display, window, ExposureMask | KeyPressMask);
+//
+    ///* display the window */
+    //XMapWindow(display, window);
+//
+    //XFlush(display);
+    //GC gc = XCreateGC(display, window, 0, NULL);
+//
+	//for(int i = 0; i < IMAGE_BUFFER_COUNT; ++i)
+	//	ImageProcessing::images[i] = XCreateImage(display, DefaultVisual(display, 0), 24, ZPixmap, 0, ImageProcessing::imageBuffer[i].data(), IMAGE_WIDTH, IMAGE_HEIGHT, 32, 0);
+//
 
 
 
@@ -93,10 +93,10 @@ int main()
 breakout:
 
     /* destroy window */
-    XDestroyWindow(display, window);
+    //XDestroyWindow(display, window);
 
     /* close connection to server */
-    XCloseDisplay(display);
+    //XCloseDisplay(display);
 	
 
 
