9372df5339c7c4aa5560cc50c65aadffc189d390
Iliocostalis <manuel.kohler.tt@gmail.com>
2022-07-20 19:51:32 +0200

    update

--- imageProcessing.h
+++ imageProcessing.h
@@ -12,8 +12,8 @@
 namespace ImageProcessing
 {
     extern std::array<std::vector<char>, IMAGE_BUFFER_COUNT> imageBuffer;
-    //extern std::atomic<int> currentImageIndex = 0;
-    //extern std::array<XImage*, IMAGE_BUFFER_COUNT> images;
+    extern std::atomic<int> currentImageIndex = 0;
+    extern std::array<XImage*, IMAGE_BUFFER_COUNT> images;
 
     extern void init();
     extern void process(uint8_t* data, size_t size);
