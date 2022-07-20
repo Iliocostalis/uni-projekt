54c9b8455973da58c627c4fe8384165c0e49988b
Iliocostalis <manuel.kohler.tt@gmail.com>
2022-07-20 15:57:00 +0200

    update

--- cam.cpp
+++ cam.cpp
@@ -49,6 +49,9 @@ void* threadFunc(void* arg)
 {
     while(threadRunning.load(std::memory_order_acquire))
     {
+		if(!threadRunning.load(std::memory_order_acquire))
+			return (void*)nullptr;
+			
 		auto now = std::chrono::high_resolution_clock::now();
 		auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now - old);
     	old = now;
