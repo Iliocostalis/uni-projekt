317465b52dbce15c533e972be6b81bfbc9dde79d
Iliocostalis <manuel.kohler.tt@gmail.com>
2022-07-20 17:53:19 +0200

    update

--- main.cpp
+++ main.cpp
@@ -3,8 +3,47 @@
 #include <iomanip>
 #include <thread>
 
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+static void glfwError(int id, const char* description)
+{
+  std::cout << description << std::endl;
+}
+
 int main()
 {
+
+	glfwSetErrorCallback(&glfwError);
+    int res = glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(1000, 1000, "MyWindow", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    glViewport(0, 0, 1000, 1000);
+
+
+    while(!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();    
+    }
+
+    glfwTerminate();
+    
+    std::cout << "Test" << std::endl;
+
+	return 0;
+
 	Cam cam;
 	cam.init();
 	cam.start();
