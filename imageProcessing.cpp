9fd8fccc4d68347c624c3fbede72288b26276ef2
Iliocostalis <manuel.kohler.tt@gmail.com>
2022-07-20 19:49:37 +0200

    update

--- imageProcessing.cpp
+++ imageProcessing.cpp
@@ -12,27 +12,27 @@ namespace ImageProcessing
     std::chrono::microseconds lasts[5];
     int ind = 0;
 
-    std::array<std::vector<char>, IMAGE_BUFFER_COUNT> imageBuffer;
-    std::atomic<int> currentImageIndex = 0;
-    std::array<XImage*, IMAGE_BUFFER_COUNT> images;
+    //std::array<std::vector<char>, IMAGE_BUFFER_COUNT> imageBuffer;
+    //std::atomic<int> currentImageIndex = 0;
+    //std::array<XImage*, IMAGE_BUFFER_COUNT> images;
 
     void init()
     {
-        for(auto& image : imageBuffer)
-            image.resize(1280 * 960 * 4);
+        //for(auto& image : imageBuffer)
+        //    image.resize(1280 * 960 * 4);
     }
 
     void process(uint8_t* data, size_t size)
     {
-        int nextImageIndex = (currentImageIndex + 1) % imageBuffer.size();
-        for(int i = 0; i < 1280 * 960; ++i)
-        {
-            imageBuffer[nextImageIndex][i * 4 + 0] = data[i];
-            imageBuffer[nextImageIndex][i * 4 + 1] = data[i];
-            imageBuffer[nextImageIndex][i * 4 + 2] = data[i];
-            imageBuffer[nextImageIndex][i * 4 + 3] = data[i];
-        }
-        currentImageIndex = nextImageIndex;
+        //int nextImageIndex = (currentImageIndex + 1) % imageBuffer.size();
+        //for(int i = 0; i < 1280 * 960; ++i)
+        //{
+        //    imageBuffer[nextImageIndex][i * 4 + 0] = data[i];
+        //    imageBuffer[nextImageIndex][i * 4 + 1] = data[i];
+        //    imageBuffer[nextImageIndex][i * 4 + 2] = data[i];
+        //    imageBuffer[nextImageIndex][i * 4 + 3] = data[i];
+        //}
+        //currentImageIndex = nextImageIndex;
 
 
 
--- imageProcessing.h
+++ imageProcessing.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
 #include <stdlib.h>
-#include <X11/Xlib.h>
+//#include <X11/Xlib.h>
 
 #include <array>
 #include <vector>
@@ -11,9 +11,9 @@
 
 namespace ImageProcessing
 {
-    extern std::array<std::vector<char>, IMAGE_BUFFER_COUNT> imageBuffer;
-    extern std::atomic<int> currentImageIndex = 0;
-    extern std::array<XImage*, IMAGE_BUFFER_COUNT> images;
+    //extern std::array<std::vector<char>, IMAGE_BUFFER_COUNT> imageBuffer;
+    //extern std::atomic<int> currentImageIndex = 0;
+    //extern std::array<XImage*, IMAGE_BUFFER_COUNT> images;
 
     extern void init();
     extern void process(uint8_t* data, size_t size);
