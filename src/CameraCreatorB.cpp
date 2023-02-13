4d012d060427d385606734a7406129ce0baeac77
Manu <manuel.kohler.tt@gmail.com>
2023-02-13 19:14:07 +0100

    u

--- src/CameraCreatorB.cpp
+++ src/CameraCreatorB.cpp
@@ -2,21 +2,7 @@
 //#include <PiCam.h>
 //#include <DevCam.h>
 
-#include <Config.h>
-#include <queue>
-#include <memory>
-#include <list>
-#include <functional>
-#include <atomic>
-#include <thread>
-#include <vector>
-#include <string>
-#include <map>
 #include <libcamera/libcamera.h>
-#include <condition_variable>
-#include <ICamera.h>
-
-#include <CameraCreatorB.h>
 
 #include <CameraCreatorB.h>
 
