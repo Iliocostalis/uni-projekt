9e63f1dc53ff8772ba10c9122d54c48ae1ee65cc
Manu <manuel.kohler.tt@gmail.com>
2023-02-13 19:12:37 +0100

    u

--- src/CameraCreatorB.cpp
+++ src/CameraCreatorB.cpp
@@ -1,6 +1,23 @@
 #include <Utils.h>
 //#include <PiCam.h>
 //#include <DevCam.h>
+
+#include <Config.h>
+#include <queue>
+#include <memory>
+#include <list>
+#include <functional>
+#include <atomic>
+#include <thread>
+#include <vector>
+#include <string>
+#include <map>
+#include <libcamera/libcamera.h>
+#include <condition_variable>
+#include <ICamera.h>
+
+#include <CameraCreatorB.h>
+
 #include <CameraCreatorB.h>
 
 ICamera* CameraCreatorB::getCamera() {
